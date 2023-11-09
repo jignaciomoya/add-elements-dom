d4df329813bac724b2ed1220d127d361d472c01c
jignaciomoya <joseignaciomoyajim@gmail.com>
2023-11-09 19:19:46 +0100

    entrega definitiva

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
  // Aquí tu código
+
+ const agregar = document.getElementById('agregar');
+ const añadirTexto = document.getElementById('lista');
+
+ agregar.addEventListener('click', function(){
+    const textoUno = prompt('Introduzca su elemento:');
+    añadirTexto.innerHTML = textoUno;
+ })
